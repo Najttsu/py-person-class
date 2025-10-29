3dd237856e2bbeeff3a65d7e442068fc03760eae
Bohdan Kutsyi <bodikoen@gmail.com>
2025-10-29 11:51:29 +0200

    Added Person.people.close() and made fix

--- app/main.py
+++ app/main.py
@@ -4,17 +4,18 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     persons_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if person.get("wife", 0):
+        if person.get("wife"):
             husband = Person.people[person["name"]]
             husband.wife = Person.people[person["wife"]]
-        elif person.get("husband", 0):
+        elif person.get("husband"):
             wife = Person.people[person["name"]]
             wife.husband = Person.people[person["husband"]]
 
